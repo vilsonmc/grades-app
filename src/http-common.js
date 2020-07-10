6906992c1088ca89fbc4b97898051ca694799433
vilsonmc <vilsonmaximo@yahoo.com.br>
2020-07-10 00:32:59 -0300

    Adição de comentários

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-//Define a URL base da origem para consumo do servico
+//Define a URL base da origem (API) para consumo do servico
 export default axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
